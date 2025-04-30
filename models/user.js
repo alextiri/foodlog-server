77f4f3e33b213c8e765c734b9d97eba3d4470847
Tiri <tirisca.alex@gmail.com>
2025-04-30 14:09:38 +0300

    This is the signin endpoint

--- models/user.js
+++ models/user.js
@@ -2,21 +2,19 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
       // define association here
     }
   }
   User.init(
     {
-      email: {
-        DataTypes: STRING,
-        allowNull: false,
-        unique: true,
-      },
-      password: {
-        DataTypes: STRING,
-        allowNull: false,
-      },
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
     },
     {
       sequelize,
